2790e84ae314a53ef42e4d4a5e04be9b85b45dd9
selva0st <101826704+selva0st@users.noreply.github.com>
2022-03-17 20:23:17 +0200

    Add files via upload

---
+++ userbot/assistant/id.py
@@ -0,0 +1,32 @@
+from telethon import events
+from telethon.utils import pack_bot_file_id
+
+
+@tgbot.on(events.NewMessage(pattern="/id"))
+async def _(event):
+    if event.reply_to_msg_id:
+        await event.get_input_chat()
+        r_msg = await event.get_reply_message()
+        if r_msg.media:
+            bot_api_file_id = pack_bot_file_id(r_msg.media)
+            await tgbot.send_message(
+                event.chat_id,
+                "ايـدي الـدردشة: `{}`\nايدي المستخدم: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id), bot_api_file_id
+                ),
+            )
+        else:
+            await tgbot.send_message(
+                event.chat_id,
+                "ايـدي الـدردشة: `{}`\nايدي المستخدم: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id)
+                ),
+            )
+    else:
+        await tgbot.send_message(
+            event.chat_id, "ايـدي الـدردشة: `{}`".format(str(event.chat_id))
+        )
+
+
+# 𝗦𝙾𝚄𝚁𝙲𝙴 𝗦𝙴𝙻𝚅𝙰
+# @ttccss
